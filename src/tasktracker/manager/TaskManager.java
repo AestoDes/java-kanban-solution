aaa5f5f160b0d45abb451ca927019df36b259f90
Aesto <marat.grus@gmail.com>
2024-12-16 05:30:34 +0300

    Добавлен метод getSubtasksOfEpic для TaskManager и его реализация

--- src/tasktracker/manager/TaskManager.java
+++ src/tasktracker/manager/TaskManager.java
@@ -10,12 +10,21 @@ public interface TaskManager {
     int generateId();
 
     void createTask(Task task);
-
     void createEpic(Epic epic);
-
     void createSubtask(Subtask subtask);
 
     List<Task> getAllTasks();
-
     List<Epic> getAllEpics();
+
+    List<Subtask> getSubtasksOfEpic(int epicId); // Добавлено
+
+    Task getTask(int id);
+
+    void updateTask(Task task);
+    void updateSubtask(Subtask subtask);
+
+    void deleteTaskById(int id);
+    void deleteEpicById(int id);
+
+    List<Task> getHistory();
 }
