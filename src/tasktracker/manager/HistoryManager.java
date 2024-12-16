067e24f7fb09ff71af5e115e44a53654fb828272
Aesto <marat.grus@gmail.com>
2024-12-16 05:18:24 +0300

    Исправлены ошибки CheckStyle: добавлены пустые строки между методами в HistoryManager

--- src/tasktracker/manager/HistoryManager.java
+++ src/tasktracker/manager/HistoryManager.java
@@ -5,7 +5,10 @@ import tasktracker.model.Task;
 import java.util.List;
 
 public interface HistoryManager {
+    
     void add(Task task);
+
     List<Task> getHistory();
+
     void remove(int id); // Новый метод для удаления задачи по ID
 }
